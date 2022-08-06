d887f00eeb02b73912d323c5472892d35b2d2c99
isaac <isaacd4444@gmail.com>
2022-08-06 02:02:32 -0400

    built app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'
+import Title from './components/Title'
+import Modal from './components/Modal';
+import EventList from './components/EventList';
+import NewEventForm from './components/NewEventForm';
 
 function App() {
+  const [showModal,setshowModal]=useState(false);
+  const [showevents, setShowEvents]=useState(true);
+  const [events, setEvents]=useState([])
+  
+  const addEvent=(event)=>{
+    setEvents(
+      (prevEvents)=>{
+        return[...prevEvents,event]
+      }
+    )
+setshowModal(false);
+  }
+  const handleClick=(id)=>{
+    setEvents((prevevents)=>{
+      return prevevents.filter((event)=>{
+      return event.id!=id;
+    })})
+  
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Title title="Events in your Area" subtitle="The Latest!"/>
+      
+      
+      <div>
+        {showevents && <button onClick={()=>setShowEvents(false)}>Hide Events</button>}
+        {(showevents==false) && <button onClick={()=>setShowEvents(true)}>Show Events</button>}
+      </div>
+    
+      {showevents && <EventList eventlist={events} handleClick={handleClick}/>}
+
+      <div className='button-wrapper'><button className="show-deal" onClick={()=>{setshowModal(true)}}>Add New Event</button></div>
+
+      {showModal &&
+      <Modal >
+        <NewEventForm  addEvent={addEvent}/>
+      </Modal>
+}
     </div>
   );
 }
